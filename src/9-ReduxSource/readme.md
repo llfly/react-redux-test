7c5b8358e80beb3b70a6158be6c7f819ea5996db
llfly <lflylfy@gmail.com>
2017-06-29 20:14:16 +0800

    read createStore

---
+++ src/9-ReduxSource/readme.md
@@ -0,0 +1,7 @@
+version : 3.6.0
+applyMiddlewar.js 使用自定义的 middleware 来扩展 Redux
+bindActionCreators.js 把 action creators 转成拥有同名 keys 的对象，使用时可以直接调用
+combineReducers.js 对 reducer 函数 进行拆分，拆分后的每一块独立负责管理 state 的一部分
+compose.js 从右到左来组合多个函数，函数式中常用
+createStore.js 创建一个 Redux Store 来放所有的state
+utils/warnimng.js 控制台输出一个警告
---
+++ src/9-ReduxSource/test/Store.js
@@ -0,0 +1,73 @@
+const Store = require('redux');
+
+// console.log(Store);
+
+// { __esModule: true,
+//   createStore: [Function: createStore],
+//   combineReducers: [Function: combineReducers],
+//   bindActionCreators: [Function: bindActionCreators],
+//   applyMiddleware: [Function: applyMiddleware],
+//   compose: [Function: compose] }
+
+
+
+const reducer = (state, action) => {
+    if (typeof state === 'undefined') {
+        return 0;
+    }
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        case 'CLEAR':
+            return 0;
+        default:
+            return state;
+    }
+};
+
+const store = Store.createStore(reducer);
+
+// console.log(store);
+// { dispatch: [Function: dispatch],
+//   subscribe: [Function: subscribe],
+//   getState: [Function: getState],
+//   replaceReducer: [Function: replaceReducer] }
+
+const update1 = () => {
+    console.log('update1:',store.getState());
+};
+
+const update2 = () => {
+    console.log('update2:',store.getState());
+}
+
+store.dispatch({type:'INCREMENT'});
+store.subscribe(update1);
+store.dispatch({type:'INCREMENT'});
+store.subscribe(update1);//反复监听
+// update1:2
+
+store.replaceReducer(function(state,action){
+    switch (action.type){
+        default:
+            return 100;
+    }
+});
+// update1:2
+// update1:100
+// update1:100
+
+
+store.subscribe(update2);
+
+store.dispatch({type:'INCREMENT'});
+
+// update1: 2
+// update1: 100
+// update1: 100
+
+// update1: 100
+// update1: 100
+// update2: 100
